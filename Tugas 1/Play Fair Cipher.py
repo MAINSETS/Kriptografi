b0b051b87246809ab14773275f34d4ce7c5689f4
Christopher Jie <christopherjie123@gmail.com>
2023-01-31 09:27:59 +0700

    ok

--- Tugas 1/Play Fair Cipher.py
+++ Tugas 1/Play Fair Cipher.py
@@ -1,161 +1,135 @@
-from pydoc import plain
 import random as rd
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 
-def remove(string):
-    return string.replace(" ","")
-
-alphabets=['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-
-def getkey(text):
-    key=[]
-    for i in text:
-        if i not in key:
-            key.append(i)
-
-    for i in alphabets:
-        if i not in key:
-            key.append(i)
-    return key
-
-def getmatrix(key):
-    matrix=[[]]
-    # print(key)
-    i=0
-    while(i<5):
-        m=[]
-        for j in key[i*5:(i+1)*5]:
-            m.append(j)
-        matrix.append(m)
-        i+=1
-    # print(matrix)
-    return matrix
-    
-
-def createplain(text):
-    plaintext=[]
-    i=0
-    if(len(text)%2!=0):
-        text+=rd.choice(alphabets)
-    while(i<(len(text))):
-        plaintext.append(text[i:i+2])
-        i+=2
-    return plaintext
-
-def dipslaymatrix(matrix):
-    for i in range(1,6):
-        print(matrix[i])
-        '\n'
-
-def checkmat(start,matrix):
-    for i,x in enumerate(matrix):
-        if start in x:
-            return i,x.index(start)+1
-
-def changeplain(row,col,matrix):
-    return matrix[row][col-1]
-
+#fungsi untuk mengenkripsi pesan
 def encryption():
-    msg=entry.get()
-    text=keyy.get()
-    key=getkey(text)
-    matrix=getmatrix(key)
-    plain=createplain(msg)
-    start=[]
-    cipher=[]
-    end=[]
-    for i in plain:
-        start.append(i[0])
-        end.append(i[1])
-    print("plaintext:")
-    dipslaymatrix(matrix)
-    print(start,end)
-    for i in range(int(len(plain))):
-        r1,c1=checkmat(start[i],matrix)
-        r2,c2=checkmat(end[i],matrix)
-        if r1==r2:
-            if c1==5 :
-                cipher.append(changeplain(r1,c1-4,matrix))
-                cipher.append(changeplain(r2,c2+1,matrix))
-            elif c2==5:
-                cipher.append(changeplain(r1,c1+1,matrix))
-                cipher.append(changeplain(r2,c2-4,matrix))
+    if len(plain.get()) == 0:
+        messagebox.showerror("Error", "Plain text is empty")
+    else:
+        key = keyy.get()
+        key = key.upper()
+        key = key.replace(" ", "")
+        key = list(key)
+        key = list(dict.fromkeys(key))
+        key = "".join(key)
+        key = key.replace("J", "I")
+        key = list(key)
+        alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+        alphabet = list(alphabet)
+        for i in key:
+            alphabet.remove(i)
+        key = key + alphabet
+        matrix = [key[0:5], key[5:10], key[10:15], key[15:20], key[20:25]]
+        msg1 = plain.get()
+        msg1 = msg1.upper()
+        msg1 = msg1.replace(" ", "")
+        msg1 = msg1.replace("J", "I")
+        msg1 = list(msg1)
+        i = 0
+        while i < len(msg1):
+            if msg1[i] == msg1[i + 1]:
+                msg1.insert(i + 1, "X")
+            i = i + 2
+        if len(msg1) % 2 != 0:
+            msg1.append("X")
+        i = 0
+        while i < len(msg1):
+            a = 0
+            b = 0
+            c = 0
+            d = 0
+            for j in range(5):
+                for k in range(5):
+                    if msg1[i] == matrix[j][k]:
+                        a = j
+                        b = k
+                    if msg1[i + 1] == matrix[j][k]:
+                        c = j
+                        d = k
+            if a == c:
+                if b == 4:
+                    b = -1
+                if d == 4:
+                    d = -1
+                msg1[i] = matrix[a][b + 1]
+                msg1[i + 1] = matrix[c][d + 1]
+            elif b == d:
+                if a == 4:
+                    a = -1
+                if c == 4:
+                    c = -1
+                msg1[i] = matrix[a + 1][b]
+                msg1[i + 1] = matrix[c + 1][d]
             else:
-                cipher.append(changeplain(r1,c1+1,matrix))
-                cipher.append(changeplain(r2,c2+1,matrix))
-        elif c1==c2:
-            if r1==5 :
-                cipher.append(changeplain(r1-4,c1,matrix))
-                cipher.append(changeplain(r2+1,c2,matrix))
-            elif r2==5:
-                cipher.append(changeplain(r1+1,c1,matrix))
-                cipher.append(changeplain(r2-4,c2,matrix))
-            else:
-                cipher.append(changeplain(r1+1,c1,matrix))
-                cipher.append(changeplain(r2+1,c2,matrix))
-        else:
-            cipher.append(changeplain(r1,c2,matrix))
-            cipher.append(changeplain(r2,c1,matrix))
-    label=Label(frame,text="Encryption:" ,padx=5,pady=5)
-    label.grid(row=3,column=0)
-    label=Label(frame,text=cipher ,padx=5,pady=5)
-    label.grid(row=3,column=1)
-    return cipher
-
-def change(row,col,matrix):
-    return matrix[row][col-1]
-
+                msg1[i] = matrix[a][d]
+                msg1[i + 1] = matrix[c][b]
+            i = i + 2
+        msg1 = "".join(msg1)
+        cipher.set(msg1)
+        cipherfive.set(fiveletters(msg1))
+        
+#fungsi untuk mendekripsi pesan
 def decryption():
-    msg=encryption()
-    text=keyy.get()
-    key=getkey(text)
-    matrix=getmatrix(key)
-    plain=createplain(msg)
-    start=[]
-    cipher=[]
-    end=[]
-    for i in plain:
-        start.append(i[0])
-        end.append(i[1])
-    print("ciphertext:")
-    dipslaymatrix(matrix)
-    print(start,end)
-    for i in range(int(len(plain))):
-        r1,c1=checkmat(start[i],matrix)
-        r2,c2=checkmat(end[i],matrix)
-        if r1==r2:
-            print("same row",r1,c1,r2,c2)
-            if c1==1 :
-                cipher.append(change(r1,c1+4,matrix))
-                cipher.append(change(r2,c2-1,matrix))
-            elif c2==1:
-                cipher.append(change(r1,c1-1,matrix))
-                cipher.append(change(r2,c2+4,matrix))
+    if len(plain.get()) == 0:
+        messagebox.showerror("Error", "Plain text is empty")
+    else:
+        key = keyy.get()
+        key = key.upper()
+        key = key.replace(" ", "")
+        key = list(key)
+        key = list(dict.fromkeys(key))
+        key = "".join(key)
+        key = key.replace("J", "I")
+        key = list(key)
+        alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+        alphabet = list(alphabet)
+        for i in key:
+            alphabet.remove(i)
+        key = key + alphabet
+        matrix = [key[0:5], key[5:10], key[10:15], key[15:20], key[20:25]]
+        msg1 = plain.get()
+        msg1 = msg1.upper()
+        msg1 = msg1.replace(" ", "")
+        msg1 = msg1.replace("J", "I")
+        msg1 = list(msg1)
+        i = 0
+        while i < len(msg1):
+            a = 0
+            b = 0
+            c = 0
+            d = 0
+            for j in range(5):
+                for k in range(5):
+                    if msg1[i] == matrix[j][k]:
+                        a = j
+                        b = k
+                    if msg1[i + 1] == matrix[j][k]:
+                        c = j
+                        d = k
+            if a == c:
+                if b == 0:
+                    b = 5
+                if d == 0:
+                    d = 5
+                msg1[i] = matrix[a][b - 1]
+                msg1[i + 1] = matrix[c][d - 1]
+            elif b == d:
+                if a == 0:
+                    a = 5
+                if c == 0:
+                    c = 5
+                msg1[i] = matrix[a - 1][b]
+                msg1[i + 1] = matrix[c - 1][d]
             else:
-                cipher.append(change(r1,c1-1,matrix))
-                cipher.append(change(r2,c2-1,matrix))
-        elif c1==c2:
-            print("same col",r1,c1,r2,c2)
-            if r1==1 :
-                cipher.append(change(r1+4,c1,matrix))
-                cipher.append(change(r2-1,c2,matrix))
-            elif r2==1:
-                cipher.append(change(r1-1,c1,matrix))
-                cipher.append(change(r2+4,c2,matrix))
-            else:
-                cipher.append(change(r1-1,c1,matrix))
-                cipher.append(change(r2-1,c2,matrix))
-        else:
-            print(r1,c1,r2,c2)
-            cipher.append(change(r1,c2,matrix))
-            cipher.append(change(r2,c1,matrix))
-    label=Label(frame,text="Decryption:" ,padx=5,pady=5)
-    label.grid(row=4,column=0)
-    label=Label(frame,text=cipher,padx=5,pady=5)
-    label.grid(row=4,column=1)
+                msg1[i] = matrix[a][d]
+                msg1[i + 1] = matrix[c][b]
+            i = i + 2
+        msg1 = "".join(msg1)
+        plain.set(msg1)
+        plainfive.set(fiveletters(msg1))
+
 
 #untuk membuka file teks dan langsung ada di kotak teks
 def openfiletxt():
@@ -195,68 +169,68 @@ def fiveletters(text):
 #menghapus semua kotak
 def clear():
     keyy.set("")
-    msg.set("")
+    plain.set("")
     plainfive.set("")
     cipher.set("")
     cipherfive.set("")
 
 #keluar dari program
 def exit():
-    frame.destroy()
+    root.destroy()
 
-playfair=Tk()
+root=Tk()
 
-msg = StringVar()
+plain = StringVar()
 keyy = StringVar()
 cipher = StringVar()
 plainfive = StringVar()
 cipher = StringVar()
 cipherfive = StringVar()
 
-label1=Label(playfair,text="Playfair Cipher" ,padx=5,pady=5)
-label1.pack(padx=30,pady=30)
-frame=LabelFrame(playfair,text="",padx=60,pady=60)
-frame.pack(padx=30,pady=30)
+label1 = Label(root, text="Key")
+label1.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+entry1 = Entry(root, textvariable=keyy)
+entry1.grid(row=0, column=1, sticky=W, padx=5, pady=5)
 
-label2=Label(frame,text="Enter message" ,padx=5,pady=5)
-label2.grid(row=1,column=0)
-entry=Entry(frame)
-entry.grid(row=1,column=1)
+label2 = Label(root, text="Plain Text")
+label2.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+entry2 = Entry(root, textvariable=plain)
+entry2.grid(row=1, column=1, sticky=W, padx=5, pady=5)
 
-label3=Label(frame,text="Enter key" ,padx=5,pady=5)
-label3.grid(row=2,column=0)
-keyy=Entry(frame)
-keyy.grid(row=2,column=1)
+label3 = Label(root, text="Plain Text 5 Letters Group")
+label3.grid(row=1, column=2, sticky=W, padx=5, pady=5)
+entry3 = Entry(root, textvariable=plainfive)
+entry3.grid(row=1, column=3, sticky=W, padx=5, pady=5)
 
-label4 = Label(frame, text="Plain Text 5 Letters Group")
-label4.grid(row=1, column=2, sticky=W, padx=5, pady=5)
-entry4 = Entry(frame, textvariable=plainfive)
-entry4.grid(row=1, column=3, sticky=W, padx=5, pady=5)
+label4 = Label(root, text="Cipher Text")
+label4.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+entry4 = Entry(root, textvariable=cipher)
+entry4.grid(row=2, column=1, sticky=W, padx=5, pady=5)
 
-label5 = Label(frame, text="Cipher Text 5 Letters Group")
+label5 = Label(root, text="Cipher Text 5 Letters Group")
 label5.grid(row=2, column=2, sticky=W, padx=5, pady=5)
-entry5 = Entry(frame, textvariable=cipherfive)
+entry5 = Entry(root, textvariable=cipherfive)
 entry5.grid(row=2, column=3, sticky=W, padx=5, pady=5)
 
-button1=Button(frame,text="Encrypt",command=encryption)
+button1=Button(root,text="Encrypt",command=encryption)
 button1.grid(row=1,column=5)
 
-button2=Button(frame,text="Decrypt",command=decryption)
+button2=Button(root,text="Decrypt",command=decryption)
 button2.grid(row=2,column=5)
 
-button3 = Button(frame, text="Open Text File", command=openfiletxt)
+button3 = Button(root, text="Open Text File", command=openfiletxt)
 button3.grid(row=4, column=0, sticky=W, padx=5, pady=5)
 
-button4 = Button(frame, text="Open Binary File", command=openfilebiner)
+button4 = Button(root, text="Open Binary File", command=openfilebiner)
 button4.grid(row=4, column=1, sticky=W, padx=5, pady=5)
 
-button5 = Button(frame, text="Save", command=savefile)
+button5 = Button(root, text="Save", command=savefile)
 button5.grid(row=4, column=2, sticky=W, padx=5, pady=5)
 
-button6 = Button(frame, text="Clear", command=clear)
+button6 = Button(root, text="Clear", command=clear)
 button6.grid(row=5, column=0, sticky=W, padx=5, pady=5)
 
-button7 = Button(frame, text="Exit", command=exit)
+button7 = Button(root, text="Exit", command=exit)
 button7.grid(row=5, column=1, sticky=W, padx=5, pady=5)
 
-playfair.mainloop()
+root.mainloop()
