3e938a93fed89169fac029e1bc3feaa3f2536c99
Christopher Jie <christopherjie123@gmail.com>
2023-02-03 21:59:38 +0700

    mantap

--- Tugas 1/Enigma Cipher.py
+++ Tugas 1/Enigma Cipher.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import string
 from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 
 def encrypt():
     plaintext = input_entry.get()
@@ -17,7 +19,8 @@ def encrypt():
             ciphertext += enigma[char]
         else:
             ciphertext += char
-    output_label["text"] = ciphertext
+    output.set(ciphertext)
+    output.set(fiveletters(ciphertext))
 
 def decrypt():
     ciphertext = input_entry.get()
@@ -31,12 +34,11 @@ def decrypt():
     plaintext = ""
     for char in ciphertext:
         if char in alphabet:
-            plaintext += alphabet[(alphabet.index(char) - 3) % 26]
-            plaintext += alphabet[(alphabet.index(plaintext[-1]) - 2) % 26]
-            plaintext += alphabet[(alphabet.index(plaintext[-1]) - 1) % 26]
+            plaintext += alphabet[(alphabet.index(char)) % 26]
         else:
             plaintext += char
     input.set(plaintext)
+    input.set(fiveletters(plaintext))
 
 def displayrotors():
     rotors = [3,2,1] 
@@ -46,7 +48,17 @@ def displayrotors():
         enigma[alphabet[i]] = alphabet[(i + rotors[0]) % 26]
         enigma[alphabet[i]] = alphabet[(alphabet.index(enigma[alphabet[i]]) + rotors[1]) % 26]
         enigma[alphabet[i]] = alphabet[(alphabet.index(enigma[alphabet[i]]) + rotors[2]) % 26]
-    output_label2["text"] = enigma
+    messagebox.showinfo("text", enigma)
+
+def fiveletters(text):
+    newtext = ""
+    count = 0
+    for i in text:
+        if count%5 == 0:
+            newtext = newtext + " "
+        newtext = newtext + i
+        count = count + 1
+    return newtext
 
 #menghapus semua kotak
 def clear():
@@ -63,6 +75,9 @@ root.geometry("1150x400")
 root.resizable(0, 0)
 
 input = StringVar()
+output = StringVar()
+plainfive = StringVar()
+cipherfive = StringVar()
 
 input_label = tk.Label(root, text="Plain Text:")
 input_label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
@@ -90,8 +105,17 @@ input_label2.grid(row=2, column=0, sticky=W, padx=5, pady=5)
 
 output_label = tk.Label(root)
 output_label.grid(row=2, column=1, sticky=W, padx=5, pady=5)
+output2 = Entry(root, textvariable=output)
+output2.grid(row=2, column=1, sticky=W, padx=5, pady=5)
 
-output_label2 = tk.Label(root)
-output_label2.grid(row=5, column=1, sticky=W, padx=5, pady=5)
+label3 = Label(root, text="Plain Text 5 Letters Group")
+label3.grid(row=0, column=2, sticky=W, padx=5, pady=5)
+entry3 = Entry(root, textvariable=plainfive, state=DISABLED)
+entry3.grid(row=0, column=3, sticky=W, padx=5, pady=5)
+
+label4 = Label(root, text="Cipher Text 5 Letters Group")
+label4.grid(row=1, column=2, sticky=W, padx=5, pady=5)
+entry4 = Entry(root, textvariable=cipherfive, state=DISABLED)
+entry4.grid(row=1, column=3, sticky=W, padx=5, pady=5)
 
 root.mainloop()
