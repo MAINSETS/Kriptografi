4e33ea7003eba7facd3b5939fc6969be1965922d
Vito <82307940+VitoAdinugroho@users.noreply.github.com>
2023-02-03 22:28:26 +0700

    gui fix enigma

--- Tugas 1/Enigma Cipher.py
+++ Tugas 1/Enigma Cipher.py
@@ -7,6 +7,7 @@ from tkinter import messagebox
 def encrypt():
     plaintext = input_entry.get()
     plaintext = plaintext.upper()
+    plaintext = removeNonASCII(removeSpaces(toUpperCase(plaintext)))
     alphabet = string.ascii_uppercase
     enigma = {}
     for i in range(len(alphabet)):
@@ -20,11 +21,12 @@ def encrypt():
         else:
             ciphertext += char
     output.set(ciphertext)
-    output.set(fiveletters(ciphertext))
+    cipherfive.set(fiveletters(ciphertext))
 
 def decrypt():
     ciphertext = input_entry.get()
     ciphertext = ciphertext.upper()
+    ciphertext = removeNonASCII(removeSpaces(toUpperCase(ciphertext)))
     alphabet = string.ascii_uppercase
     enigma = {}
     for i in range(len(alphabet)):
@@ -38,7 +40,7 @@ def decrypt():
         else:
             plaintext += char
     input.set(plaintext)
-    input.set(fiveletters(plaintext))
+    plainfive.set(fiveletters(plaintext))
 
 def displayrotors():
     rotors = [3,2,1] 
@@ -48,7 +50,7 @@ def displayrotors():
         enigma[alphabet[i]] = alphabet[(i + rotors[0]) % 26]
         enigma[alphabet[i]] = alphabet[(alphabet.index(enigma[alphabet[i]]) + rotors[1]) % 26]
         enigma[alphabet[i]] = alphabet[(alphabet.index(enigma[alphabet[i]]) + rotors[2]) % 26]
-    messagebox.showinfo("text", enigma)
+    messagebox.showinfo("Rotors", enigma)
 
 def fiveletters(text):
     newtext = ""
@@ -60,10 +62,56 @@ def fiveletters(text):
         count = count + 1
     return newtext
 
+#fungsi mengubah semua text menjadi upper case
+def toUpperCase(text):
+	return text.upper()
+
+#fungsi menghapus spasi
+def removeSpaces(text):
+	newtext = ""
+	for i in text:
+		if i == " ":
+			continue
+		else:
+			newtext = newtext + i
+	return newtext
+
+#fungsi menghapus karakter non-alfabet 
+def removeNonASCII(text):
+    text.upper()
+    newtext = ""
+    for i in text:
+        if i not in string.ascii_uppercase:
+            continue
+        else:
+            newtext = newtext + i
+    return newtext
+
+#untuk membuka file teks dan langsung ada di kotak teks
+def openfiletxt():
+    file = filedialog.askopenfile(mode='r', filetypes=[('Text files', 'txt')])
+    if file is not None:
+        content = file.read(10000)
+        input.set(content)
+
+#fitur untuk menyimpan file teks jika sudah dienkripsi/didekripsi
+def savefile():
+    if len(input.get()) == 0:
+        messagebox.showerror("Error", "Plain text is empty")
+    else:
+        file = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if file is not None:
+            file.write(input.get())
+            file.close()
+            
 #menghapus semua kotak
 def clear():
     input_entry.delete(0, END)
     output_label["text"] = ""
+    input.set("")
+    output.set("")
+    plainfive.set("")
+    cipherfive.set("")
 
 #keluar dari program
 def exit():
@@ -71,7 +119,7 @@ def exit():
 
 root = tk.Tk()
 root.title("Enigma Cipher")
-root.geometry("1150x400")
+root.geometry("700x400")
 root.resizable(0, 0)
 
 input = StringVar()
@@ -98,7 +146,7 @@ exit_button = tk.Button(root, text="Exit", command=exit)
 exit_button.grid(row=6, column=1, sticky=W, padx=5, pady=5)
 
 rotors_button = tk.Button(root, text="Display Rotors", command=displayrotors)
-rotors_button.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+rotors_button.grid(row=4, column=1, sticky=W, padx=5, pady=5)
 
 input_label2 = tk.Label(root, text="Cipher Text:")
 input_label2.grid(row=2, column=0, sticky=W, padx=5, pady=5)
@@ -117,5 +165,11 @@ label4 = Label(root, text="Cipher Text 5 Letters Group")
 label4.grid(row=1, column=2, sticky=W, padx=5, pady=5)
 entry4 = Entry(root, textvariable=cipherfive, state=DISABLED)
 entry4.grid(row=1, column=3, sticky=W, padx=5, pady=5)
+
+button3 = Button(root, text="Open Text File", command=openfiletxt)
+button3.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+
+button5 = Button(root, text="Save Cipher", command=savefile)
+button5.grid(row=4, column=2, sticky=W, padx=5, pady=5)
 
 root.mainloop()
